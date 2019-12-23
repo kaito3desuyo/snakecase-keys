1d9881109f51b109c4dab763af4a48b5490d5629
kaito3desuyo <admin@seapolis.me>
2019-12-23 17:30:52 +0900

    compile as umd module
    Signed-off-by: kaito3desuyo <admin@seapolis.me>

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
+const BabelEnginePlugin = require("babel-engine-plugin");
+
+module.exports = {
+  mode: process.env.NODE_ENV || "development",
+  entry: "./index.js",
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        use: [{ loader: "babel-loader" }],
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".js"]
+  },
+  output: {
+    filename: "index.js",
+    path: path.resolve(__dirname, "dist"),
+    library: "snakecase-keys",
+    libraryTarget: "umd",
+    libraryExport: "default",
+    globalObject: "typeof self !== 'undefined' ? self : this"
+  },
+  plugins: [
+    new CopyPlugin([
+      {
+        from: "./index.d.ts",
+        to: "./index.d.ts"
+      }
+    ]),
+    new BabelEnginePlugin()
+  ]
+};
